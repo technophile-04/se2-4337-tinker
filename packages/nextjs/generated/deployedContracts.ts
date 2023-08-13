e1fab68a4136d1cdc8c7fc1733e7b65a6dcd032c
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-13 17:16:21 +0530

    commit before adding alchemy/aa-sdk

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -1,3 +1,89 @@
-const deployedContracts = null;
+const deployedContracts = {
+  "5": [
+    {
+      name: "goerli",
+      chainId: "5",
+      contracts: {
+        SimpleAccountFactory: {
+          address: "0x9406Cc6185a346906296840746125a0E44976454",
+          abi: [
+            {
+              inputs: [
+                {
+                  internalType: "contract IEntryPoint",
+                  name: "_entryPoint",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "constructor",
+            },
+            {
+              inputs: [],
+              name: "accountImplementation",
+              outputs: [
+                {
+                  internalType: "contract SimpleAccount",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "owner",
+                  type: "address",
+                },
+                {
+                  internalType: "uint256",
+                  name: "salt",
+                  type: "uint256",
+                },
+              ],
+              name: "createAccount",
+              outputs: [
+                {
+                  internalType: "contract SimpleAccount",
+                  name: "ret",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "owner",
+                  type: "address",
+                },
+                {
+                  internalType: "uint256",
+                  name: "salt",
+                  type: "uint256",
+                },
+              ],
+              name: "getAddress",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
 
 export default deployedContracts;
